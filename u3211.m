4af65167fe542aaa391f753f9514c25558a8affd
Alessandro Castelanelli Oddo <caste.1998@gmail.com>
2025-03-21 11:02:38 +0100

    Update u3211 in function form

--- u3211.m
+++ u3211.m
@@ -1,22 +1,29 @@
-clear all, close all, clc
-% %function [u]
-% 
-% ts = 0.0040;
+function [u3211] = u3211(A,Ts)
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% Estimation and Learning in Aerospace Project A.Y. 24-25 
+% Function creating a sequence u3211 with amplitude as "A" at sample time
+% "Ts" in a total time of 8 with an ammount of N samples
 
+% Outputs : u3211 : [N x 2] Sequence u3211
+
+% Authors:  Alessandro Castelanelli Oddo (alessandro.castelanelli@polimi.it)
+%            (@polimi.it)                     
+%            (@polimi.it)                                                   
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 % Define time values
 % t = [0 1    3    5    6    7 8]; 
-t = [0:0.0040:8]';
+t = [0:Ts:8]';
 % u = [0 0.1 -0.1  0.1 -0.1  0 0];
-sec1 = ones((1/0.004),1);
-sec3 = ones((3/0.004),1);
-sec2 = ones((2/0.004),1);
-sec05 = ones((0.5/0.004),1);
+sec1 = ones((1/Ts),1);
+sec3 = ones((3/Ts),1);
+sec2 = ones((2/Ts),1);
+sec05 = ones((0.5/Ts),1);
 
 u0 = 0.*sec05;
-u1 = 0.1*sec3;
-u2 = -0.1*sec2;
-u3 = 0.1*sec1;
+u1 = A*sec3;
+u2 = -A*sec2;
+u3 = A*sec1;
 u4 = -u3;
 u5 = u0;
 
@@ -30,4 +37,6 @@ xlabel('Time [s]');
 ylabel('u');
 grid on;
 
-u3211 = [t,u]
+u3211 = [t,u];
+
+end
